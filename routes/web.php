70822727319b5f55aed987a35c24ea1196110c0b
yusufnur <yusuf.n@mail.ugm.ac.id>
2017-11-02 16:44:54 +0700

    well

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('template', function () {
 	return view('coba1');
 });
 
-Route::get('coba1', function () {
+Route::get('coba1', function () { 
 	return view('coba1');
 });
 
@@ -43,4 +43,18 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+/*
 Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+
+Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+
+Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+ // kalau dikelompokkan jadi yg dibawah 
+*/
+
+Route::prefix('admin')->group(function() {
+	Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+	Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+	Route::get('/', 'AdminController@index')->name('admin.dashboard');
+});
+
