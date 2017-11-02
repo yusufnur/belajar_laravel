ed2e00113af24ce8a7c48842e9830d5546ea2cc2
yusufnur <yusuf.n@mail.ugm.ac.id>
2017-11-02 17:05:12 +0700

    well

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,6 +59,16 @@ class Handler extends ExceptionHandler
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
+		
+		$guard = array_get($exception->guards(), 0);
+			switch ($guard) {
+			case 'admin':
+			$login = 'admin.login';
+			break;
+			default:
+			$login = 'login';
+			break;
+		}
 
         return redirect()->guest(route('login'));
     }
